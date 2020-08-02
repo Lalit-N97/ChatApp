b1f169cd5d24fedea7e9b7b8e5967656c150dc0a
pirate <lalitnegi1997@gmail.com>
2020-08-02 22:26:31 +0530

    frontend is ready

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -1,60 +1,69 @@
-import React, { useEffect, useState, useReducer } from "react";
-import io from "socket.io-client";
+import React, { useState, useEffect } from "react";
 import queryString from "query-string";
-import "./Chat.css";
+import io from "socket.io-client";
+
+import TextContainer from "../text/TextContainer";
+import Messages from "../messages/Messages";
 import InfoBar from "../infobar/InfoBar";
+import Input from "../input/Input";
+
+import "./Chat.css";
 
 let socket;
-const ENDPOINT = "localhost:5000";
 
-const Chat = (props) => {
+const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
+  const ENDPOINT = "https://react-chat-appserver.herokuapp.com/";
 
   useEffect(() => {
-    const { name, room } = queryString.parse(props.location.search);
-    setName(name);
-    setRoom(room);
+    const { name, room } = queryString.parse(location.search);
+
     socket = io(ENDPOINT);
+
+    setRoom(room);
+    setName(name);
+
     socket.emit("join", { name, room }, (error) => {
-      props.history.push("/");
-      alert(error);
+      if (error) {
+        alert(error);
+      }
     });
-    return () => {
-      socket.emit("disconnect");
-      socket.off();
-    };
-  }, [ENDPOINT, props.location.search]);
+  }, [ENDPOINT, location.search]);
 
   useEffect(() => {
     socket.on("message", (message) => {
-      setMessages([...messages, message]);
+      setMessages((messages) => [...messages, message]);
     });
-  }, [messages]);
+
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+  }, []);
 
   const sendMessage = (event) => {
     event.preventDefault();
+
     if (message) {
       socket.emit("sendMessage", message, () => setMessage(""));
     }
-    console.log(message, messages);
   };
 
   return (
     <div className="outerContainer">
       <div className="container">
         <InfoBar room={room} />
-        {/* <input
-          type="text"
-          value={message}
-          onChange={(event) => setMessage(event.target.value)}
-          onKeyPress={(event) =>
-            event.key === "Enter" ? sendMessage(event) : null
-          }
-        /> */}
+        <Messages messages={messages} name={name} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
       </div>
+      <TextContainer users={users} />
     </div>
   );
 };
--- client/src/components/infobar/InfoBar.jsx
+++ client/src/components/infobar/InfoBar.jsx
@@ -1,27 +1,25 @@
 import React from "react";
 import "./InfoBar.css";
 
-const InfoBar = (props) => {
-  return (
-    <div className="infobar">
-      <div className="leftInnerContainer">
-        <img
-          className="onlineIcon"
-          src="https://raw.githubusercontent.com/adrianhajdin/project_chat_application/master/client/src/icons/onlineIcon.png"
-          alt="online image"
-        />
-        <h3>{props.room}</h3>
-      </div>
-      <div className="rightInnerContainer">
-        <a href="/">
-          <img
-            src="https://raw.githubusercontent.com/adrianhajdin/project_chat_application/master/client/src/icons/closeIcon.png"
-            alt="close image"
-          />
-        </a>
-      </div>
+const InfoBar = ({ room }) => (
+  <div className="infoBar">
+    <div className="leftInnerContainer">
+      <img
+        className="onlineIcon"
+        src="https://raw.githubusercontent.com/adrianhajdin/project_chat_application/master/client/src/icons/onlineIcon.png"
+        alt="online icon"
+      />
+      <h3>{room}</h3>
     </div>
-  );
-};
+    <div className="rightInnerContainer">
+      <a href="/">
+        <img
+          src="https://raw.githubusercontent.com/adrianhajdin/project_chat_application/master/client/src/icons/closeIcon.png"
+          alt="close icon"
+        />
+      </a>
+    </div>
+  </div>
+);
 
 export default InfoBar;
